bb1dc4b4773f1e91ed05a959af02fda092bffefc
Pasztor Adam <adam.psztr@icloud.com>
2022-04-09 12:37:27 +0200

    all file upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.btn');
+
+let ripple;
+
+btn.addEventListener('mouseenter',(e)=>{
+	//console.log(e.clientX);
+	//console.log(e.target.getBoundingClientRect());
+	const left = e.clientX - e.target.getBoundingClientRect().left;
+	const top = e.clientY - e.target.getBoundingClientRect().top;
+
+	ripple = document.createElement('div');
+	ripple.classList.add("ripple");
+	ripple.style.left = `${left}px`;
+	ripple.style.top = `${top}px`;
+	btn.prepend(ripple);
+});
+
+btn.addEventListener('mouseleave', () => {
+	btn.removeChild(ripple);
+})
